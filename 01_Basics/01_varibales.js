d8bb1599b92d23ddf0908581c6a261873715bcf6
Shreyas Sahane <shreyassahane55@gmail.com>
2025-09-13 09:53:18 +0530

    01-02

---
+++ 01_Basics/01_varibales.js
@@ -0,0 +1,19 @@
+/*
+const, let, var
+comments, multi line comments
+*/
+const accountId=2142
+let accountEmail= "shreyassahane55@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=23 //not allowed to change const keyword
+accountEmail="sad@gmail.com"
+accountPassword="32r2341"
+accountCity="pune"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var,
+because of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+// alert("Hello") we are using nodejs not browser
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+/*
+***PRIMITIVE***
+number => 2 to power 53
+bigint =>
+string => ""
+boolean =>true/false
+null => standalone value
+undefined 
+symbol => for uniqueness
+*/
+
+/*
+***OBJECTS***
+*/
+console.log(typeof("Hitesh"))
+console.log(typeof(undefined))
+console.log(typeof(null))
